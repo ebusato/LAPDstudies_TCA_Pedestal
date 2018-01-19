662b9025c5f2b3164de7b115649cde7090081295
Busato <emmanuel.busato@gmail.com>
2018-01-19 11:13:42 +0100

    update

--- SRout.C
+++ SRout.C
@@ -7,21 +7,38 @@ void SRout() {
   f->Print();
   TTree* t = (TTree*) f->Get("tree");
 
-  TCanvas* c = new TCanvas("c", "c", 800, 1200);
+  TCanvas* c = new TCanvas("c", "c", 1200, 1200);
   int nrows = 6;
-  int ncols = 2;
+  int ncols = 3;
   c->Divide(ncols,nrows);
-  for(int i=1; i<=ncols*nrows; i++) {
-    c->cd(i);
+  int j=1;
+  for(int i=1; i<=nrows; i++) {
+    TString cut("Evt == ");
+    cut+=i*10;
+    cout << cut << endl;
+    c->cd(j);
     gPad->SetTopMargin(0);
     gPad->SetBottomMargin(0.15);
-    TString cut("Evt == ");
-    cut+=i;
-    cout << cut << endl;
-    if(i%2!=0) {  
-      t->Draw("Pulse[0]:SampleTimes", cut.Data());
-    } else {
-      t->Draw("Pulse[0]:CapaId[0]", cut.Data());
-    }
+    t->Draw("Pulse[0]:SampleTimes", cut.Data());
+    t->Scan("SRout[0]", cut.Data());
+    int SRout = t->GetLeaf("SRout")->GetValue(0);
+    cout << "SRout = " << SRout << endl;
+    PutText(0.5, 0.9, kBlack, cut.Data(), 0.07);
+    TString SRoutString("SRout=");
+    SRoutString+=SRout;
+    PutText(0.65, 0.9, kBlack, SRoutString.Data(), 0.07);
+    c->cd(j+1);
+    gPad->SetTopMargin(0);
+    gPad->SetBottomMargin(0.15);
+    t->Draw("Pulse[0]:SampleIndices", cut.Data());
+    PutText(0.5, 0.9, kBlack, cut.Data(), 0.07); 
+    PutText(0.65, 0.9, kBlack, SRoutString.Data(), 0.07);
+    c->cd(j+2);
+    gPad->SetTopMargin(0);
+    gPad->SetBottomMargin(0.15);
+    t->Draw("Pulse[0]:CapaId[0]", cut.Data());
+    PutText(0.5, 0.9, kBlack, cut.Data(), 0.07); 
+    PutText(0.65, 0.9, kBlack, SRoutString.Data(), 0.07);
+    j+=3;
   }
 }
