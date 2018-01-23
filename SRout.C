58a8e5e8095a9c792d506c9464979c29a4ab24af
Busato <emmanuel.busato@gmail.com>
2018-01-23 15:09:43 +0100

    update

--- SRout.C
+++ SRout.C
@@ -7,6 +7,12 @@ void SRout() {
   f->Print();
   TTree* t = (TTree*) f->Get("tree");
 
+  /////////////////////////////////////////////
+  // Implement conversion to in automatically
+  TString noPulse("8");
+  int noPulseI(8);
+  /////////////////////////////////////////////
+
   TCanvas* c = new TCanvas("c", "c", 1200, 1200);
   int nrows = 6;
   int ncols = 3;
@@ -15,13 +21,14 @@ void SRout() {
   for(int i=1; i<=nrows; i++) {
     TString cut("Evt == ");
     cut+=i*10;
+    //cut+=i*1;
     cout << cut << endl;
     c->cd(j);
     gPad->SetTopMargin(0);
     gPad->SetBottomMargin(0.15);
-    t->Draw("Pulse[0]:SampleTimes", cut.Data());
-    t->Scan("SRout[0]", cut.Data());
-    int SRout = t->GetLeaf("SRout")->GetValue(0);
+    t->Draw("Pulse["+noPulse+"]:SampleTimes", cut.Data());
+    t->Scan("SRout["+noPulse+"]", cut.Data());
+    int SRout = t->GetLeaf("SRout")->GetValue(noPulseI);
     cout << "SRout = " << SRout << endl;
     PutText(0.5, 0.9, kBlack, cut.Data(), 0.07);
     TString SRoutString("SRout=");
@@ -30,13 +37,13 @@ void SRout() {
     c->cd(j+1);
     gPad->SetTopMargin(0);
     gPad->SetBottomMargin(0.15);
-    t->Draw("Pulse[0]:SampleIndices", cut.Data());
+    t->Draw("Pulse["+noPulse+"]:SampleIndices", cut.Data());
     PutText(0.5, 0.9, kBlack, cut.Data(), 0.07); 
     PutText(0.65, 0.9, kBlack, SRoutString.Data(), 0.07);
     c->cd(j+2);
     gPad->SetTopMargin(0);
     gPad->SetBottomMargin(0.15);
-    t->Draw("Pulse[0]:CapaId[0]", cut.Data());
+    t->Draw("Pulse["+noPulse+"]:CapaId["+noPulse+"]", cut.Data());
     TLine* line = new TLine(SRout, 0, SRout, 500);
     line->Draw();
     PutText(0.5, 0.9, kBlack, cut.Data(), 0.07); 
